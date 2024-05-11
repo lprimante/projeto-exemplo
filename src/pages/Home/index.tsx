24d18173916639f86f38d69580ceeb5672d05d36
Luciana Primante <luciana.primante@dasa.com.br>
2024-05-11 10:40:05 -0300

    configure UI components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,8 @@
 import { useEffect, useState } from 'react'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import Checkbox from '@mui/material/Checkbox'
 
 interface Todos {
     id: number
@@ -8,6 +12,17 @@ interface Todos {
 export const Home = () => {
     const [todos, setTodos] = useState<Array<Todos>>([])
 
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => {
+        const todoSelected = parseInt(event.target.id)
+        const updateTodos: Array<Todos> = todos.map(todo => {
+            if (todo.id === todoSelected) {
+                todo.completed = checked
+            }
+            return todo
+        })
+        setTodos(updateTodos)
+    }
+
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/todos')
             .then(response => response.json())
@@ -15,13 +30,18 @@ export const Home = () => {
     }, [])
 
     return (
-        <>
+        <Container>
+            <Typography variant="h1" component="h1" sx={{ mb: 2 }}>
+                Minha Lista de Tarefas
+            </Typography>
             {todos.map(todo => (
-                <div key={todo.id}>
-                    <h1>{todo.title}</h1>
-                    <p>{todo.completed}</p>
-                </div>
+                <Box sx={{ my: 4, display: 'flex' }} key={todo.id}>
+                    <Checkbox checked={todo.completed} onChange={handleChange} name={todo.title} id={todo.id.toString()} />
+                    <Typography variant="h6" component="p" sx={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+                        {todo.title}
+                    </Typography>
+                </Box>
             ))}
-        </>
+        </Container>
     )
 }
--- src/pages/__tests__/index.snapshot.spec.tsx
+++ src/pages/__tests__/index.snapshot.spec.tsx
@@ -4,5 +4,15 @@ import { Home } from '..'
 
 test('renders learn react link', () => {
     const tree = renderer.create(<Home />).toJSON()
-    expect(tree).toMatchInlineSnapshot(`null`)
+    expect(tree).toMatchInlineSnapshot(`
+<div
+  className="MuiContainer-root MuiContainer-maxWidthLg css-1oqqzyl-MuiContainer-root"
+>
+  <h1
+    className="MuiTypography-root MuiTypography-h1 css-l4yn1p-MuiTypography-root"
+  >
+    Minha Lista de Tarefas
+  </h1>
+</div>
+`)
 })
